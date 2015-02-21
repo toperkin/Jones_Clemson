f592e5ad2ff966216c3a74c8834d2af87a42cda7
toperkin <toperkin@gmail.com>
2015-02-21 12:58:39 -0600

    update

--- csv_to_graphs.py
+++ csv_to_graphs.py
@@ -7,24 +7,25 @@ df = pd.read_csv('clogged_cap.csv', index_col=0)
 LENGTH_OF_ROLLING_MEAN = 10
 
 mg = df[(284.8 < df.wavelength) & (df.wavelength <= 285.3)].mean()
+mg = mg.drop('wavelength')
 mg_mean = pd.rolling_mean(mg, LENGTH_OF_ROLLING_MEAN, center=True)
-mg_mean = mg_mean.drop('wavelength')
 
 oh = df[(309 < df.wavelength) & (df.wavelength <= 309.99)].mean()
+oh = oh.drop('wavelength')
 oh_mean = pd.rolling_mean(oh, LENGTH_OF_ROLLING_MEAN, center=True)
-oh_mean = oh_mean.drop('wavelength')
 
 ag328 = df[(327.4 < df.wavelength) & (df.wavelength <= 328.4)].mean()
+ag328 = ag328.drop('wavelength')
 ag328_mean = pd.rolling_mean(ag328, LENGTH_OF_ROLLING_MEAN, center=True)
-ag328_mean = ag328_mean.drop('wavelength')
 
 ag338 = df[(337.9 < df.wavelength) & (df.wavelength <= 338.4)].mean()
+ag338 = ag338.drop('wavelength')
 ag338_mean = pd.rolling_mean(ag338, LENGTH_OF_ROLLING_MEAN, center=True)
-ag338_mean = ag338_mean.drop('wavelength')
 
 cs = df[(852 < df.wavelength) & (df.wavelength <= 852.4)].mean()
+cs = cs.drop('wavelength')
 cs_mean = pd.rolling_mean(cs, LENGTH_OF_ROLLING_MEAN, center=True)
-cs_mean = cs_mean.drop('wavelength')
+
 
 plt.figure(figsize=(12, 9))
 plt.xlabel('Seconds')
